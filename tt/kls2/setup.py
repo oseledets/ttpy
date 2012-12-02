818d778f8c23529335280f353ff28785aee1f787
Ivan Oseledets <ivan.oseledets@gmail.com>
2012-12-02 23:36:17 +0400

    Preparation for the new (Cython-based) code for the simple KLS scheme. Algorithm: 1) Write a pure Python code 2) Cythonize it

---
+++ tt/kls2/setup.py
@@ -0,0 +1,30 @@
+#This script will build the main subpackages  
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration, get_info
+    config = Configuration('kls', parent_package, top_path)
+    #import ipdb; ipdb.set_trace()
+    #config.add_library()
+    tt_dir = '../tt-fort/'
+
+    src = ['putstrmodule.F90','dispmodule.f90','matrix_util.f90','ttals.f90','dlacn1.f','dlapst.f','dlarpc.f',
+           'zlacn1.f','expokit.f','explib.f90','normest.f90','tt_kls.f90']
+    src = [tt_dir + x for x in src]
+    src.append('tt_kls.pyf')
+    config.add_extension('dyn_tt',sources=src,include_dirs=[tt_dir])
+    #config.add_extension['
+    return config
+    
+#from distutils.core import setup
+#from numpy.distutils.core import setup, Extension
+#src = ['tt_f90.f90','tt_f90.pyf']
+#inc_dir = ['tt-fort']
+#lib = ['tt-fort/mytt.a']
+#ext = Extension('tt_f90', src, include_dirs=inc_dir)
+#setup(ext_modules = [ext])
+
+
+if __name__ == '__main__':
+    print 'This is the wrong setup.py to run'
+
+
+#, include_dirs=None, define_macros=None, undef_macros=None, library_dirs=None, libraries=None, runtime_library_dirs=None, extra_objects=None, extra_compile_args=None, extra_link_args=None, export_symbols=None, swig_opts=None, depends=None, language=None, f2py_options=None, module_dirs
