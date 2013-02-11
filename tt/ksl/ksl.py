ea5278b037997e919eb5ef9b0604385b4c3ce132
Ivan Oseledets <ivan.oseledets@gmail.com>
2013-02-11 16:03:32 +0400

    Template for the KSL scheme

---
+++ tt/ksl/ksl.py
@@ -0,0 +1,47 @@
+""" Dynamical TT-approximation """
+import numpy as np
+import dyn_tt
+from tt import tensor
+def ksl(A,y0,tau,rmax=150,kickrank=5,verb=1,nswp=10):
+    """ Dynamical TT-approximation """
+    ry = y0.r.copy()
+    #lam = np.zeros(ry[y0.d])
+    #for i in xrange(10):
+    #Check for dtype
+    y = tensor()
+    if np.iscomplex(A.tt.core).any() or np.iscomplex(y0.core).any():
+        dyn_tt.dyn_tt.ztt_ksl(y0.d,A.n,A.m,A.tt.r,A.tt.core +0j, y0.core+0j, ry, tau, rmax, kickrank, nswp, verb)
+        y.core = dyn_tt.dyn_tt.zresult_core.copy()    
+    else:
+        A.tt.core = np.real(A.tt.core)
+        y0.core = np.real(y0.core) 
+        dyn_tt.dyn_tt.tt_ksl(y0.d,A.n,A.m,A.tt.r,A.tt.core, y0.core, ry, tau, rmax, kickrank, nswp, verb)
+        y.core = dyn_tt.dyn_tt.result_core.copy()
+    dyn_tt.dyn_tt.deallocate_result()
+    y.d = y0.d
+    y.n = A.n.copy()
+    y.r = ry 
+    y.get_ps()
+    return y
+"""
+def ksl(A,y0,tau,rmax=150,kickrank=5,verb=1,nswp=10):
+    ry = y0.r.copy()
+    #lam = np.zeros(ry[y0.d])
+    #for i in xrange(10):
+    #Check for dtype
+    y = tensor()
+    if np.iscomplex(A.tt.core).any() or np.iscomplex(y0.core).any():
+        dyn_tt.dyn_tt.ztt_ksl(y0.d,A.n,A.m,A.tt.r,A.tt.core +0j, y0.core+0j, ry, tau, rmax, kickrank, nswp, verb)
+        y.core = dyn_tt.dyn_tt.zresult_core.copy()    
+    else:
+        A.tt.core = np.real(A.tt.core)
+        y0.core = np.real(y0.core) 
+        dyn_tt.dyn_tt.tt_ksl(y0.d,A.n,A.m,A.tt.r,A.tt.core, y0.core, ry, tau, rmax, kickrank, nswp, verb)
+        y.core = dyn_tt.dyn_tt.result_core.copy()
+    dyn_tt.dyn_tt.deallocate_result()
+    y.d = y0.d
+    y.n = A.n.copy()
+    y.r = ry 
+    y.get_ps()
+    return y
+ """
--- tt/ksl/setup.py
+++ tt/ksl/setup.py
@@ -1,25 +1,32 @@
-from setuptools import setup, find_packages
+#This script will build the main subpackages  
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration, get_info
+    config = Configuration('kls', parent_package, top_path)
+    #import ipdb; ipdb.set_trace()
+    #config.add_library()
+    tt_dir = '../tt-fort/'
+    exp_dir = '../tt-fort/expm'
+    src = ['ttals.f90','tt_ksl.f90']
+    exp_src = ['explib.f90', 'normest.f90','expokit.f','dlacn1.f','dlapst.f','dlarpc.f','zlacn1.f']
+    exp_src = [exp_dir + '/' + x for x in exp_src]
+    config.add_library('expokit',sources=exp_src)
+    src = [tt_dir + x for x in src]
+    src.append('tt_ksl.pyf')
+    config.add_extension('dyn_tt',sources=src,depends=['print_lib','expokit','mytt'],
+			libraries=['print_lib','expokit','mytt'],)
+    return config
+    
 #from distutils.core import setup
-from distutils.extension import Extension
-import numpy as np
-
-# we'd better have Cython installed, or it's a no-go
-try:
-    from Cython.Distutils import build_ext
-except:
-    print "You don't seem to have Cython installed. Please get a"
-    print "copy from www.cython.org and install it"
-    sys.exit(1)
-
-ksl = Extension("ksl", ["ksl.pyx"], extra_compile_args = ["-O3", "-Wall", "-undefined,dynamic_lookup"], include_dirs = [np.get_include()])
+#from numpy.distutils.core import setup, Extension
+#src = ['tt_f90.f90','tt_f90.pyf']
+#inc_dir = ['tt-fort']
+#lib = ['tt-fort/mytt.a']
+#ext = Extension('tt_f90', src, include_dirs=inc_dir)
+#setup(ext_modules = [ext])
 
 
-extensions = [ksl]
+if __name__ == '__main__':
+    print 'This is the wrong setup.py to run'
 
-# finally, we can pass all this to distutils
-setup(
-    name="ksl",
-    packages=find_packages(),
-    ext_modules=extensions,
-    cmdclass = {'build_ext': build_ext},
-)
+
+#, include_dirs=None, define_macros=None, undef_macros=None, library_dirs=None, libraries=None, runtime_library_dirs=None, extra_objects=None, extra_compile_args=None, extra_link_args=None, export_symbols=None, swig_opts=None, depends=None, language=None, f2py_options=None, module_dirs
