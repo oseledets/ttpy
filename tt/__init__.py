404c4094d59e875cae575eeaad928ee883952fa5
Ivan Oseledets <ivan.oseledets@gmail.com>
2013-02-20 15:41:04 +0400

    An attempt to make more universal BLAS loading

--- tt/__init__.py
+++ tt/__init__.py
@@ -1,1 +1,12 @@
-from core.tt import *
+try: 
+    from core.tt import *
+except:
+    import ctypes
+    try:
+        ctypes.CDLL("libmkl_rt.so", ctypes.RTLD_GLOBAL)
+    except:
+        try:
+            ctypes.CDLL("liblapack.so.3", ctypes.RTLD_GLOBAL)
+        except:
+            print "Did not find MKL or LAPACK library"
+    from core.tt import *
