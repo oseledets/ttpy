502878fc170e36c2daf7ba29f16ed50fa153cb05
Ivan Oseledets <ivan.oseledets@gmail.com>
2013-03-02 22:37:54 +0400

    The new version, which now includes the second-order ksl scheme

--- hh_hermite2.py
+++ hh_hermite2.py
@@ -9,27 +9,40 @@ import tt
 import time
 from math import pi,sqrt
 import quadgauss
-import os
-import sys
-
+import mctdh
+import dvr
 
 f = 4 #The number of degrees of freedom
-L = 7 #The domain is [-L, L], periodic
 lm = 0.111803 #The magic constant
 #lm = 0 #The magic constant
 
 #lm = 
 N = 20 # The size of the spectral discretization
 
-x, ws = quadgauss.cdgqf(N,6,0,0.5) #Generation of hermite quadrature
+#x, ws = quadgauss.cdgqf(N,6,0,0.5) #Generation of hermite quadrature
 #Generate Laplacian
-lp = np.zeros((N,N))
-for i in xrange(N):
-    for j in xrange(N):
-        if i is not j:
-            lp[i,j] = (-1)**(i - j)*(2*(x[i] - x[j])**(-2) - 0.5)
-        else:
-            lp[i,j] = 1.0/6*(4*N - 1 - 2 * x[i]**2)
+#lp = np.zeros((N,N))
+#for i in xrange(N):
+#    for j in xrange(N):
+#        if i is not j:
+#            lp[i,j] = (-1)**(i - j)*(2*(x[i] - x[j])**(-2) - 0.5)
+#        else:
+#            lp[i,j] = 1.0/6*(4*N - 1 - 2 * x[i]**2)
+#lp = tt.matrix(lp)
+
+trafo, x, dif2, dif1 = mctdh.initho(N, 1.0, 0.0, 0.0, 0.0, '')
+basis = 1 # HO Basis
+rpbaspar = np.zeros(3, dtype = np.int)
+rpbaspar[0] = 0.0 #hoxeq
+rpbaspar[1] = 1.0 #hofreq = rpbaspar(2) #ho
+rpbaspar[2] = 1.0 #homass = rpbaspar(3) #homass
+ipbaspar = []
+ipbaspar1 = []
+lsq = False
+lnw = False
+weight = dvr.dvrweights(trafo, x, basis, rpbaspar, ipbaspar, ipbaspar1, lsq, lnw)
+
+lp = -dif2
 lp = tt.matrix(lp)
 e = tt.eye([N])
 
@@ -74,34 +87,47 @@ for s in xrange(f-1):
 A = 0.5*lp2 + tt.diag(0.5*harm + lm*V)
 A = A.round(eps) 
 
-H = 1j*A
-#Generate the initial Gaussian, which is just shifted
 
-gs = np.exp(-0.5*(x-2)**2)
+
+H = 1j * A
+#Generate the initial Gaussian, which is just shifted
+#This is how it is defined in MCTDH
+#q1      gauss  2.0  0.0  0.7071
+x0 = 2.0
+p0 = 0.0
+dx = 0.7071
+
+gs = np.exp(-0.25*((x-x0)/dx)**2) * np.exp(1j * p0 * (x - x0))
+gs = gs * weight
 gs = tt.tensor(gs,1e-8)
 start = None
 for i in xrange(f):
     start = tt.kron(start,gs)
-radd = 20
+radd = 5
 start = start+0*tt.rand(start.n,start.d,radd)
+start = start*(1.0/start.norm())
 y = start.copy()
 print 'initial value norm:', start.norm()
 cf = []
-tf = 150.0
-nit = 5000
-tau = (tf/nit)
+tau = 0.1;
 i = 0
 t = 0
+tf = 20
 import time
 t1 = time.time()
+#s1 = 0.86053284899368-0.46936702639161j
+#s1 = 0.99855056911186 - 0.04996850934329j
+#S = expm(tau * H.full())
+#y1 = np.dot(S, start.full().flatten('F'))
+#s2 = np.dot(np.conj(y1),start.full().flatten('F'))
+#s1 = 0.52021138426564-0.80781148446727j
+cf = {}
 while t <= tf:
     print '%f/%f' % (t,tf)
+    cf[round(t,3)] = tt.dot(y,start)
     y = ksl(H,y,tau)
-    cf.append(tt.dot(y,start))
     t += tau
 t2 = time.time()
 print("Elapsed time: %f" % (t2-t1))
 
-zz = np.abs(fft(np.conj(cf)))
-lls = np.arange(zz.size)*pi/(0.5*tf)
 
