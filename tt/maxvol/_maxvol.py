4b219566485f5c080688028dfe2b45629e091147
Ivan Oseledets <ivan.oseledets@gmail.com>
2013-03-20 11:41:32 +0400

    Fixed maxvol behaviour for n <= r

--- tt/maxvol/_maxvol.py
+++ tt/maxvol/_maxvol.py
@@ -1,4 +1,8 @@
 from maxvol import dmaxvol
+from numpy import arange, asanyarray
 
 def maxvol(a, nswp = 20, tol = 5e-2):
+    a = asanyarray(a)
+    if a.shape[0] <= a.shape[1]:
+        return arange(a.shape[0])
     return dmaxvol(a, nswp, tol)
