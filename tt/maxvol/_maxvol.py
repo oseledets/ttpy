de6445d1df41058d5af287e0c002abd45a6b3a26
Saluev <tigran.saluev@gmail.com>
2013-03-21 13:00:52 +0400

    maxvol fixed once again.

--- tt/maxvol/_maxvol.py
+++ tt/maxvol/_maxvol.py
@@ -5,4 +5,4 @@ def maxvol(a, nswp = 20, tol = 5e-2):
     a = asanyarray(a)
     if a.shape[0] <= a.shape[1]:
         return arange(a.shape[0])
-    return dmaxvol(a, nswp, tol)
+    return dmaxvol(a, nswp, tol) - 1
