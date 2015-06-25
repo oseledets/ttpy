1470e6e9a09ad7e464083ceb33b975ad1ffbc39f
Alexander Novikov <novikov@bayesgroup.ru>
2015-06-25 13:21:41 +0300

    Simplify code

--- tt/riemannian/riemannian.py
+++ tt/riemannian/riemannian.py
@@ -11,6 +11,30 @@ def reshape(a, sz):
     return np.reshape(a, sz, order="F")
 
 
+def cores_orthogonalization_step(coresX, dim, left_to_right=True):
+    """TT-Tensor X orthogonalization step.
+
+    The function can change the shape of some cores.
+    """
+    cc = coresX[dim]
+    r1, n, r2 = cc.shape
+    if left_to_right:
+        # Left to right orthogonalization step.
+        assert(0 <= dim < len(coresX) - 1)
+        cc, rr = np.linalg.qr(reshape(cc, (-1, r2)))
+        r2 = cc.shape[1]
+        coresX[dim] = reshape(cc, (r1, n, r2))
+        coresX[dim+1] = np.tensordot(rr, coresX[dim+1], 1)
+    else:
+        # Right to left orthogonalization step.
+        assert(0 < dim < len(coresX))
+        cc, rr = np.linalg.qr(reshape(cc, (r1, -1)).T)
+        r1 = cc.shape[1]
+        coresX[dim] = reshape(cc.T, (r1, n, r2))
+        coresX[dim-1] = np.tensordot(coresX[dim-1], rr.T, 1)
+    return coresX
+
+
 # Three functions below are here for debugging. They help to implement
 # the algorithms in a straightforward and inefficient manner.
 def left(X, i):
@@ -73,15 +97,13 @@ def project(X, Z, clusterSize=30, debug=False):
     # Get rid of redundant ranks (they cause technical difficulties).
     X = X.round(eps=0)
 
-    numDims = X.d
-    modeSize = X.n
+    numDims, modeSize = X.d, X.n
     coresX = tt.tensor.to_list(X)
     coresZ = [None] * len(zArr)
     for idx in xrange(len(zArr)):
         assert(modeSize == zArr[idx].n).all()
         coresZ[idx] = tt.tensor.to_list(zArr[idx])
 
-    projRanks = np.concatenate(([1], 2 * X.r[1:-1], [1]))
     # Initialize the cores of the projection_X(sum z[i]).
     coresP = []
     for dim in xrange(numDims):
@@ -96,19 +118,11 @@ def project(X, Z, clusterSize=30, debug=False):
     rhs = [[0] * (numDims+1) for _ in xrange(len(zArr))]
     for idx in xrange(len(zArr)):
         rhs[idx][numDims] = np.ones([1, 1])
-    # Right to left orthogonalization of X and preparation of the rhs vectors.
+    # Right to left sweep to orthogonalize the cores and prepare rhs.
     for dim in xrange(numDims-1, 0, -1):
-        # Orthogonalization.
-        cc = coresX[dim].copy()
-        r1, n, r2 = cc.shape
-        if dim > 0:
-            cc = reshape(cc, (r1, -1)).T
-            cc, rr = np.linalg.qr(cc)
-            r1 = cc.shape[1]
-            # Warning: since ranks can change here, do not use X.r!
-            # Use coresX[dim].shape instead.
-            coresX[dim] = reshape(cc.T, (r1, n, r2)).copy()
-            coresX[dim-1] = np.tensordot(coresX[dim-1], rr.T, 1)
+        # Right to left orthogonalization of the X cores.
+        coresX = cores_orthogonalization_step(coresX, dim, left_to_right=False)
+        r1, n, r2 = coresX[dim].shape
 
         # Fill the right orthogonal part of the projection.
         for value in xrange(modeSize[dim]):
@@ -125,75 +139,66 @@ def project(X, Z, clusterSize=30, debug=False):
     # lsh[idx] is an X.rank_dim x zArr[idx].rank_dim matrix.
     lhs = [np.ones([1, 1]) for _ in xrange(len(zArr))]
     # Left to right sweep.
-    for dim in xrange(numDims):
-        cc = coresX[dim].copy()
-        r1, n, r2 = cc.shape
-        if dim < numDims-1:
-            # Left to right orthogonalization.
-            cc = reshape(cc, (-1, r2))
-            cc, rr = np.linalg.qr(cc)
-            r2 = cc.shape[1]
-            # Warning: since ranks can change here, do not use X.r!
-            # Use coresX[dim].shape instead.
-            if debug:
-                # Need to do it before the move non orthogonal part rr to
-                # the coresX[dim+1].
-                rightQ = right(tt.tensor.from_list(coresX), dim+1)
-            coresX[dim] = reshape(cc, (r1, n, r2)).copy()
-            coresX[dim+1] = np.tensordot(rr, coresX[dim+1], 1)
+    for dim in xrange(numDims - 1):
+        if debug:
+            rightQ = right(tt.tensor.from_list(coresX), dim+1)
+        # Left to right orthogonalization of the X cores.
+        coresX = cores_orthogonalization_step(coresX, dim, left_to_right=True)
+        r1, n, r2 = coresX[dim].shape
+        cc = reshape(coresX[dim], (-1, r2))
 
-            for idx in xrange(len(zArr)):
-                currZCore = reshape(coresZ[idx][dim], (zArr[idx].r[dim], -1))
-                currPCore = np.dot(lhs[idx], currZCore)
+        for idx in xrange(len(zArr)):
+            currZCore = reshape(coresZ[idx][dim], (zArr[idx].r[dim], -1))
+            currPCore = np.dot(lhs[idx], currZCore)
 
-                # TODO: consider using np.einsum.
-                coreProd = np.tensordot(coresX[dim], coresZ[idx][dim], axes=(1, 1))
-                coreProd = np.transpose(coreProd, (0, 2, 1, 3))
-                coreProd = reshape(coreProd, (r1*zArr[idx].r[dim], r2*zArr[idx].r[dim+1]))
-                lhs[idx] = reshape(lhs[idx], (1, -1))
-                lhs[idx] = np.dot(lhs[idx], coreProd)
-                lhs[idx] = reshape(lhs[idx], (r2, zArr[idx].r[dim+1]))
+            # TODO: consider using np.einsum.
+            coreProd = np.tensordot(coresX[dim], coresZ[idx][dim], axes=(1, 1))
+            coreProd = np.transpose(coreProd, (0, 2, 1, 3))
+            coreProd = reshape(coreProd, (r1*zArr[idx].r[dim], r2*zArr[idx].r[dim+1]))
+            lhs[idx] = reshape(lhs[idx], (1, -1))
+            lhs[idx] = np.dot(lhs[idx], coreProd)
+            lhs[idx] = reshape(lhs[idx], (r2, zArr[idx].r[dim+1]))
 
-                currPCore = reshape(currPCore, (-1, zArr[idx].r[dim+1]))
-                currPCore -= np.dot(cc, lhs[idx])
-                rhs[idx][dim+1] = reshape(rhs[idx][dim+1], (zArr[idx].r[dim+1], r2))
-                currPCore = np.dot(currPCore, rhs[idx][dim+1])
-                currPCore = reshape(currPCore, (r1, modeSize[dim], r2))
-                if dim == 0:
-                    for value in xrange(modeSize[dim]):
-                        coresP[dim][0:r1, value, 0:r2] += currPCore[:, value, :]
-                else:
-                    for value in xrange(modeSize[dim]):
-                        coresP[dim][r1:, value, 0:r2] += currPCore[:, value, :]
-
-                if debug:
-                    leftQm1 = left(tt.tensor.from_list(coresX), dim-1)
-                    leftQ = left(tt.tensor.from_list(coresX), dim)
-
-                    first = np.tensordot(leftQm1.T, unfolding(zArr[idx], dim-1), 1)
-                    second = reshape(first, (-1, np.prod(modeSize[dim+1:])))
-                    if dim < numDims-1:
-                        explicit = second.dot(rightQ)
-                        orth_cc = reshape(coresX[dim], (-1, coresX[dim].shape[2]))
-                        explicit -= orth_cc.dot(leftQ.T.dot(unfolding(zArr[idx], dim)).dot(rightQ))
-                    else:
-                        explicit = second
-                    explicit = reshape(explicit, currPCore.shape)
-                    assert(np.allclose(explicit, currPCore))
-
+            currPCore = reshape(currPCore, (-1, zArr[idx].r[dim+1]))
+            currPCore -= np.dot(cc, lhs[idx])
+            rhs[idx][dim+1] = reshape(rhs[idx][dim+1], (zArr[idx].r[dim+1], r2))
+            currPCore = np.dot(currPCore, rhs[idx][dim+1])
+            currPCore = reshape(currPCore, (r1, modeSize[dim], r2))
             if dim == 0:
                 for value in xrange(modeSize[dim]):
-                    coresP[dim][0:r1, value, r2:] = coresX[dim][:, value, :]
+                    coresP[dim][0:r1, value, 0:r2] += currPCore[:, value, :]
             else:
                 for value in xrange(modeSize[dim]):
-                    coresP[dim][r1:, value, r2:] = coresX[dim][:, value, :]
+                    coresP[dim][r1:, value, 0:r2] += currPCore[:, value, :]
 
-        if dim == numDims-1:
-            for idx in xrange(len(zArr)):
-                currZCore = reshape(coresZ[idx][dim], (zArr[idx].r[dim], -1))
-                currPCore = np.dot(lhs[idx], currZCore)
-                for value in xrange(modeSize[dim]):
-                    coresP[dim][r1:, value, 0:r2] += reshape(currPCore[:, value], (r1, 1))
+            if debug:
+                leftQm1 = left(tt.tensor.from_list(coresX), dim-1)
+                leftQ = left(tt.tensor.from_list(coresX), dim)
+
+                first = np.tensordot(leftQm1.T, unfolding(zArr[idx], dim-1), 1)
+                second = reshape(first, (-1, np.prod(modeSize[dim+1:])))
+                if dim < numDims-1:
+                    explicit = second.dot(rightQ)
+                    orth_cc = reshape(coresX[dim], (-1, coresX[dim].shape[2]))
+                    explicit -= orth_cc.dot(leftQ.T.dot(unfolding(zArr[idx], dim)).dot(rightQ))
+                else:
+                    explicit = second
+                explicit = reshape(explicit, currPCore.shape)
+                assert(np.allclose(explicit, currPCore))
+
+        if dim == 0:
+            for value in xrange(modeSize[dim]):
+                coresP[dim][0:r1, value, r2:] = coresX[dim][:, value, :]
+        else:
+            for value in xrange(modeSize[dim]):
+                coresP[dim][r1:, value, r2:] = coresX[dim][:, value, :]
+
+    for idx in xrange(len(zArr)):
+        r1, n, r2 = coresX[numDims-1].shape
+        currZCore = reshape(coresZ[idx][numDims-1], (zArr[idx].r[numDims-1], -1))
+        currPCore = np.dot(lhs[idx], currZCore)
+        for value in xrange(modeSize[numDims-1]):
+            coresP[numDims-1][r1:, value, 0:r2] += reshape(currPCore[:, value], (r1, 1))
 
     if debug:
         assert(np.allclose(X.full(), tt.tensor.from_list(coresX).full()))
@@ -217,19 +222,11 @@ def projector_splitting_add(Y, delta, debug=False):
     # rhs[dim] is an (delta.rank_dim * Y.rank_dim) x 1 vector
     rhs = [None] * (numDims+1)
     rhs[numDims] = np.ones([1, 1])
-    # Right to left orthogonalization of Y and preparation of the rhs vectors.
+    # Right to left sweep to orthogonalize the cores and prepare the rhs.
     for dim in xrange(numDims-1, 0, -1):
-        # Orthogonalization.
-        cc = coresY[dim].copy()
-        r1, n, r2 = cc.shape
-        cc = reshape(cc, (r1, -1)).T
-        cc, rr = np.linalg.qr(cc)
-        r1 = cc.shape[1]
-        # Warning: since ranks can change here, do not use Y.r!
-        # Use coresY[dim].shape instead.
-        coresY[dim] = reshape(cc.T, (r1, n, r2)).copy()
-        if dim > 0:
-            coresY[dim-1] = np.tensordot(coresY[dim-1], rr.T, 1)
+        # Right to left orthogonalization of the Y cores.
+        coresY = cores_orthogonalization_step(coresY, dim, left_to_right=False)
+        r1, n, r2 = coresY[dim].shape
 
         # rhs computation.
         coreProd = np.tensordot(coresDelta[dim], coresY[dim], axes=(1, 1))
@@ -273,7 +270,7 @@ def projector_splitting_add(Y, delta, debug=False):
             lhs = reshape(lhs, (r2, delta.r[dim+1]))
 
         if dim < numDims-1:
-            # Y^- (fomula 4.7)
+            # Y^- (formula 4.7)
             s = rr - np.dot(lhs, rhs[dim+1])
             if debug:
                 first = left(tt.tensor.from_list(coresY), dim).T
